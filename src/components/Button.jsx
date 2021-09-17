fca809502a08f988ea378664c3a81da8a3c6d0b0
Gabriel Soares <gabrieldears@gmail.com>
2021-09-17 00:21:23 -0300

    commit com todo o conteudo

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Button.css'
+
+const Button = ({ children, onClick }) => {
+    return <button className="add-button" onClick={onClick}> {children}</button>;
+}
+ 
+export default Button;
